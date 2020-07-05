f0a59fd75d1619e51a45e20eefdea0a9b4304a56
Yu Hao (Tom) Lai <tomlai8882000@hotmail.com>
2020-07-05 22:46:32 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,55 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
   # write your code here!
+  
+  
+ pigeon_data = {
+  :color => {
+    :purple => ["Theo", "Peter Jr.", "Lucky"],
+    :grey => ["Theo", "Peter Jr.", "Ms. K"],
+    :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
+    :brown => ["Queenie", "Alex"]
+  },
+  :gender => {
+    :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
+    :female => ["Queenie", "Ms. K"]
+  },
+  :lives => {
+    "Subway" => ["Theo", "Queenie"],
+    "Central Park" => ["Alex", "Ms. K", "Lucky"],
+    "Library" => ["Peter Jr."],
+    "City Hall" => ["Andrew"]
+  }
+ }
+ 
+ final_result = data.each_with_object ({}) do |(key, value), final_array|
+   
+   value.each do |inner_key, names|
+     names.each do |name|
+       if !final_array[name]
+         final_array[name] = {}
+       end
+       if !final_array[name][key]
+         !final_array[name][key] = []
+       end
+       final_array[name][key].push(inner_key.to_s)
+     end
+   end
+ end
+ 
+
+ 
+
+
+
+
+
+
+
+
+
 end
+
+
+  
